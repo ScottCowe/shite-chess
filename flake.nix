96adc30c9f763862c623b35e0b58d71855b19d6e
Scott Cowe <scott.t.cowe@gmail.com>
2024-03-28 18:07:29 +0000

    Added nix dev flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Java 20 development environment with gradle";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          jdk
+          gradle
+        ];
+
+        shellHook = ''
+          exec zsh
+        '';
+      };
+    };
+}
