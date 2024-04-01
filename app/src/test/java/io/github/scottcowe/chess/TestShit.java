effc15ea6c786122a4421bd9b55ce6dee4f348cc
Scott Cowe <scott.t.cowe@gmail.com>
2024-04-01 20:33:21 +0000

    Implemented perft function

--- app/src/test/java/io/github/scottcowe/chess/TestShit.java
+++ app/src/test/java/io/github/scottcowe/chess/TestShit.java
@@ -14,4 +14,31 @@ public class TestShit {
     List<Move> moves = Position.getAllPseudoLegalMoves(pos);
     assertEquals(moves.size(), 20);
   }
+
+  @Test
+  void perftDefaultPosition() {
+    Position pos = new Position();
+
+    assertEquals(this.perft(pos, 1), 20);
+    assertEquals(this.perft(pos, 2), 400);
+  }
+
+  private int perft(Position pos, int depth) {
+    int nodes = 0;
+
+    List<Move> moves = Position.getAllPseudoLegalMoves(pos); 
+    moves = Position.removeIllegalMoves(moves);
+
+    if (depth == 1) {
+      return moves.size();
+    }
+
+    for (int i = 0; i < moves.size(); i++) {
+      Move move = moves.get(i);
+      Position newPos = pos.doMove(move);
+      nodes += this.perft(newPos, depth - 1);
+    }
+
+    return nodes;
+  }
 }
