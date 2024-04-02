57f162b935555ee0c35ab9644f44cd0e068f30d9
Scott Cowe <scott.t.cowe@gmail.com>
2024-04-02 19:46:14 +0000

    Implemented divide algorithm

--- app/src/test/java/io/github/scottcowe/chess/TestShit.java
+++ app/src/test/java/io/github/scottcowe/chess/TestShit.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Collections;
 
 import io.github.scottcowe.chess.engine.*;
 
@@ -13,9 +15,18 @@ public class TestShit {
   void perftDefaultPosition() {
     Position pos = new Position();
 
+    int depth3 = 0;
+
+    HashMap<String, Integer> depth3Divide = divide(pos, 3);
+
+    for (String s : depth3Divide.keySet()) {
+      depth3 += depth3Divide.get(s);
+      System.out.println(s + ": " + depth3Divide.get(s));
+    }
+
     int depth1 = this.perft(pos, 1);
     int depth2 = this.perft(pos, 2);
-    int depth3 = this.perft(pos, 3);
+    //int depth3 = this.perft(pos, 3);
 
     // Values from https://www.chessprogramming.org/Perft_Results
     assertEquals(depth1, 20);
@@ -37,11 +48,31 @@ public class TestShit {
 
     for (int i = 0; i < moves.size(); i++) {
       Move move = moves.get(i);
-
       Position newPos = pos.doMove(move);
+
       nodes += perft(newPos, depth - 1);
     }
 
     return nodes;
+  }
+
+  private static HashMap<String, Integer> divide(Position pos, int depth) {
+    HashMap<String, Integer> results = new HashMap<String, Integer>(); 
+
+    List<Move> moves = Position.getAllPseudoLegalMoves(pos); 
+    moves = Position.removeIllegalMoves(moves, pos);
+
+    for (int i = 0; i < moves.size(); i++) {
+      Move move = moves.get(i);
+      Position newPos = pos.doMove(move);
+
+      int perft = perft(newPos, depth - 1);
+      String moveStr = Position.getAlgebraicFromIndex(move.getFromIndex()) 
+        + Position.getAlgebraicFromIndex(move.getToIndex());
+
+      results.put(moveStr, perft);
+    }
+
+    return results;
   }
 }
