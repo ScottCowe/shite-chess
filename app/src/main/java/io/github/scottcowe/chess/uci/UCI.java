0afad413a5f7f5d705e2f6e4a4b3a54ad88e4e5f
Scott Cowe <scott.t.cowe@gmail.com>
2024-04-18 15:14:32 +0100

    Continued implementing UCI

--- app/src/main/java/io/github/scottcowe/chess/uci/UCI.java
+++ app/src/main/java/io/github/scottcowe/chess/uci/UCI.java
@@ -2,12 +2,48 @@ package io.github.scottcowe.chess.uci;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class UCI {
   private List<UCICommand> commands = new ArrayList<UCICommand>():
 
   public void recieveInput(String input) {
+    String[] split = input.trim().split("\\s+");
 
+    UCICommand command = null;
+    int argIndex = 0;
+    
+    for (int i = 0; i < split.length; i++) {
+      UCICommand cmd = this.getCommandByName(split[i]);
+
+      if (cmd != null) {
+        command = cmd;
+        argIndex = i + 1;
+        break;
+      }
+    }
+
+    if (command == null) {
+      return;
+    }
+
+    String[] requiredArgs = cmd.getArgs();
+    String[] optionalArgs = cmd.getOptionalArgs();
+    List<String> allArgs = new ArrayList<String>();
+    allArgs.addAll(Arrays.asList(requiredArgs));
+    allArgs.addAll(Arrays.asList(optionalArgs));
+
+    HashMap<String, String> args = new HashMap<String, String>();
+
+    // until next arg (optional or otherwise) is found
+    for (int i = argIndex; i < split.length; i++) {
+      if (!allArgs.contains(split[i])) {
+        continue; 
+      }
+
+
+    }
   }
 
   public void sendOutput(String output) {
@@ -20,5 +56,15 @@ public class UCI {
 
   public void init() {
 
+  }
+
+  public UCICommand getCommandByName(String name) {
+    for (UCICommand cmd : this.commands) {
+      if (cmd.getName() == name) {
+        return cmd;
+      }
+    }
+
+    return null;
   }
 }
