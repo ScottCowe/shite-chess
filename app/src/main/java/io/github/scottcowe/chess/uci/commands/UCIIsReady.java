f7e9f23fa27bab2f0dfe310ae41695dc864be5ee
Scott Cowe <scott.t.cowe@gmail.com>
2024-04-29 21:53:24 +0100

    added isready command

---
+++ app/src/main/java/io/github/scottcowe/chess/uci/commands/UCIIsReady.java
@@ -0,0 +1,15 @@
+import io.github.scottcowe.chess.uci.commands;
+
+import io.github.scottcowe.chess.uci.*;
+
+public class UCIIsReady extends UCICommand {
+  public UCIIsReady() {
+    super("isready", new String[0], new String[0]);
+  }
+
+  @Override
+  public void doStuff(HashMap<String, String> args) {
+    UCI uci = UCI.getInstance(); 
+    uci.sendOutput("readyok");
+  }
+}
